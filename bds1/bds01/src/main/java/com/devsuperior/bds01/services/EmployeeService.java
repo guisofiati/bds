564f1741bf17c0f3fb5c081dbb7430df9a91dd24
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2021-09-23 15:57:36 -0300

    #bds01: Insert employee OK

--- bds1/bds01/src/main/java/com/devsuperior/bds01/services/EmployeeService.java
+++ bds1/bds01/src/main/java/com/devsuperior/bds01/services/EmployeeService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.bds01.dto.EmployeeDTO;
+import com.devsuperior.bds01.entities.Department;
 import com.devsuperior.bds01.entities.Employee;
 import com.devsuperior.bds01.repositories.EmployeeRepository;
 
@@ -20,5 +21,15 @@ public class EmployeeService {
 	public Page<EmployeeDTO> findAll(Pageable pageable){
 		Page<Employee> page = repository.findAll(pageable);
 		return page.map(x -> new EmployeeDTO(x));
+	}
+
+	@Transactional
+	public EmployeeDTO insert(EmployeeDTO dto) {
+		Employee entity = new Employee();
+		entity.setName(dto.getName());
+		entity.setEmail(dto.getEmail());
+		entity.setDepartment(new Department(dto.getDepartmentId(), null));
+		entity = repository.save(entity);
+		return new EmployeeDTO(entity);
 	}
 }
